60b1cd6c3dabc1622d5068741be9e00d4d9b623b
Alexander Tsankov <antsankov@gmail.com>
2014-11-15 09:43:41 -0700

    Sam's shit

--- run.py
+++ run.py
@@ -13,55 +13,55 @@ import transaction
 from bank import bank
 from parser import parser
 
-# ACCOUNT_SID = "AC2a9e06ea97e4a1a785361f0e8064e870" 
-# AUTH_TOKEN = "bc96d318cbda7ef495f1418baed042f4" 
+ACCOUNT_SID = "AC2a9e06ea97e4a1a785361f0e8064e870" 
+AUTH_TOKEN = "bc96d318cbda7ef495f1418baed042f4" 
  
-# client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 app = Flask(__name__)
 
-# #this function takes in two strings and cats them together with a space. 
-# def space(return_string, input):
-#     return (return_string + " " + input)
+#this function takes in two strings and cats them together with a space. 
+def space(return_string, input):
+    return (return_string + " " + input)
 
-# def bank_init():
-#     test_bank = bank()
-#     # test_person = person('+17208378697')
-#     # test_bank.addPerson('+17208378697', test_person)
-#     return test_bank
+def bank_init():
+    test_bank = bank()
+    # test_person = person('+17208378697')
+    # test_bank.addPerson('+17208378697', test_person)
+    return test_bank
 
-# #this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
-# def returner(bank,debug):
-#     messages = client.messages.list() 
-#     for m in messages:
-#         if (m.direction == 'inbound' and debug == False):
-#             #you need to use m.from_ NOT m.From, this causes 
-#             return parser(m.from_,m.body,bank)
+#this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
+def returner(bank,debug):
+    messages = client.messages.list() 
+    for m in messages:
+        if (m.direction == 'inbound' and debug == False):
+            #you need to use m.from_ NOT m.From, this causes 
+            return parser(m.from_,m.body,bank)
 
-#         if (m.direction == 'inbound' and debug == True):
-#             #you need to use m.from_ NOT m.From, this causes keyword error  
-#             return m.body
+        if (m.direction == 'inbound' and debug == True):
+            #you need to use m.from_ NOT m.From, this causes keyword error  
+            return m.body
  
 #this is the main route with the two possible verbs, any methods after this runs automatically
 @app.route("/", methods=['GET', 'POST'])
 
 #this is the responder function 
 def responder():
-    # resp = twilio.twiml.Response()
-    # resp.message(returner(test_bank,False))
-    # return str(resp)
+    resp = twilio.twiml.Response()
+    resp.message(returner(test_bank,False))
+    return str(resp)
     return "HELLO WORLD"
 
-# @app.route("/debug", methods=['GET', 'POST'])
+@app.route("/debug", methods=['GET', 'POST'])
 
-# def debugger():
-#     resp = twilio.twiml.Response()
-#     resp.message(returner(test_bank,True))
-#     return str(resp)
+def debugger():
+    resp = twilio.twiml.Response()
+    resp.message(returner(test_bank,True))
+    return str(resp)
 
 
 #this gets the server running.
 if __name__ == "__main__":
-    #test_bank = bank_init()
+    test_bank = bank_init()
     # print (parser("1111111","withdraw 555 48758475"))
     # print (parser("2222222","deposit 555 488923478923"))
     app.run(debug=True)
