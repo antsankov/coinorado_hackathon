4a7222e4a7b77b097d3aa938c9167a9c12e2c4e7
Alexander Tsankov <antsankov@gmail.com>
2014-11-15 09:54:00 -0700

    Double asshole

--- run.py
+++ run.py
@@ -6,11 +6,10 @@ import twilio.twiml
 from twilio.rest import TwilioRestClient 
 
 #our own classes 
-# from persons import persons
+from persons import persons
 from bank import bank
-# from account import account
+from account import account
 import transaction
-from parser import parser
 
 ACCOUNT_SID = "AC2a9e06ea97e4a1a785361f0e8064e870" 
 AUTH_TOKEN = "bc96d318cbda7ef495f1418baed042f4" 
@@ -27,6 +26,90 @@ def bank_init():
     # test_person = person('+17208378697')
     # test_bank.addPerson('+17208378697', test_person)
     return test_bank
+
+
+def parser(origination_number,input,bank):
+    if(not(origination_number in bank.people.keys())):
+        test_person = persons(origination_number)
+        bank.add_person(origination_number)
+    else:
+        test_person = bank.get_person(origination_number)
+
+    #looks up the person from the bank based on their origination number
+    # user = bank.get_person(origination_number)
+    
+    #split the input 
+    mod_input = input.split()
+    verb = mod_input[0]
+    
+    #WITHDRAW
+    if (verb == "withdraw"):
+        accountID = mod_input[1]
+        amount = mod_input[2]
+
+        #check if amount is valid
+        amount = get_satoshis(amount)
+        if(amount < 0):
+            return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
+
+
+        #check if the user has the account that they want to draw from 
+        if (accountID in test_person.waccounts ):
+            
+            #get the actual account from the bank 
+            account = bank.get_account(accountID)
+
+            if (hasFunds(amount, account)):
+                withdraw(account, test_person, amount)
+                return "SUCCESS " + input
+
+        else:
+            return "Invalid/Unknown account"
+
+
+    #DEPOSIT
+    if (verb == "deposit"):
+        accountID = mod_input[1]
+        amount = mod_input[2]
+
+        #check if amount is valid
+        amount = get_satoshis(amount)
+        if(amount < 0):
+            return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
+
+        if (accountID in test_person.daccounts):
+            
+            account = bank.get_account(accountID)
+            deposit(account,test_person,amount)
+            return "SUCCESS " + input
+
+        else:
+            return "Invalid/Unknown account"
+    
+    #ADD       
+    if (verb == "add"):
+        phone_number = mod_input[1]
+        if (not(phone_number in bank.people.keys())):
+            bank.add_person(phone_number)
+            return ("Successfully added person with wallet of: " + str(bank.get_person(phone_number).address))
+        
+        else:
+            return "User has already been created"
+
+    #CREATE
+    if(verb == "create"):
+        phone_number = mod_input[1]
+        id = bank.add_account(phone_number)
+        return id 
+    else:
+        return "Unknown if withdrawl or deposit!"
+
+
+
+
+
+
+
 
 #this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
 def returner(bank,debug):
