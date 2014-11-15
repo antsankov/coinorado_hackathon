97cbaaa92e7fe78932b998e616bbb6e8898f1421
Alexander Tsankov <antsankov@gmail.com>
2014-11-14 23:55:07 -0700

    Added in origination number into the parser

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
+#the default flask for running the server
 from flask import Flask, request, redirect
+
+#this is the twiml stuff
 import twilio.twiml
 from twilio.rest import TwilioRestClient 
+
+#our own classes 
+from persons import persons
+from bank import bank
+from account import account
 
 ACCOUNT_SID = "AC2a9e06ea97e4a1a785361f0e8064e870" 
 AUTH_TOKEN = "bc96d318cbda7ef495f1418baed042f4" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
-  
 
 app = Flask(__name__)
 
@@ -14,8 +21,8 @@ app = Flask(__name__)
 def space(return_string, input):
     return (return_string + " " + input)
 
-def parser(input):
-    return_string = ""
+def parser(origination_number,input):
+    return_string = origination_number + " "
     mod_input = input.split()
 
     #this checks if we have a w or d 
@@ -63,6 +70,7 @@ def accountLookup(number):
 def hasFunds(account,amount):
     return True
 
+#talk directly to the bank
 def account_lookup(account):
     return 555
  
@@ -74,7 +82,7 @@ def returner():
     messages = client.messages.list() 
     for m in messages:
         if (m.direction == 'inbound'):
-            return parser(m.body)
+            return parser(m.From, m.body)
  
 #this is the main route with the two possible verbs 
 @app.route("/", methods=['GET', 'POST'])
@@ -88,8 +96,8 @@ def responder():
 
 #this gets the server running.
 if __name__ == "__main__":
-    print (parser("withdraw 555 48758475"))
-    print (parser("deposit 555 488923478923"))
+    print (parser("1111111","withdraw 555 48758475"))
+    print (parser("2222222","deposit 555 488923478923"))
     app.run(debug=True)
 
 
