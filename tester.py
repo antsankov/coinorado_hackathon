68a8b63cccb15a1eb4c815c1f2c7bbcee8f56872
untra <untra.sam@gmail.com>
2014-11-15 13:55:38 -0700

    tying things together

--- tester.py
+++ tester.py
@@ -19,15 +19,18 @@ def tester():
     #gives the balance
     print(parser("+17208378697","balance",test_bank))
     #add a dupilicate tester 
-    print(parser("+17208378697","add +17208378675",test_bank))
-    #creates an accoutn and prints an account number 
-    print(parser("+17208378697","create +17208378675",test_bank))
-    #creates an accoutn and prints an account number 
-    print(parser("+17208378697","create +17208378675",test_bank))
-    print(parser("+17208378697","add +57208378675",test_bank))
-    print(parser("+17208378697","info +17208378675",test_bank))
-    #print("Bank customers: " + str(test_bank.people))
-    #print("Bank accounts" + str(test_bank.accounts))
+    print(parser("+17208378697","combine 4620ffe2-86d6-4b63-ac16-4b8decfaa6f5 1KoQFBAEASCykCmnvK4kz7FRV4a1bqQu1Q" ,test_bank))
+    #gives the balance
+    print(parser("+17208378697","balance",test_bank))
+    # print(parser("+17208378697","add +17208378675",test_bank))
+    # #creates an accoutn and prints an account number 
+    # print(parser("+17208378697","create +17208378675",test_bank))
+    # #creates an accoutn and prints an account number 
+    # print(parser("+17208378697","create +17208378675",test_bank))
+    # # print(parser("+17208378697","add +57208378675",test_bank))
+    # # print(parser("+17208378697","info +17208378675",test_bank))
+    # #print("Bank customers: " + str(test_bank.people))
+    # #print("Bank accounts" + str(test_bank.accounts))
 
 
 if __name__ == "__main__":
