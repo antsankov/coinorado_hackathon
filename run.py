d8e2bd5f98aedfe114d47fc75adf56d217ea6a63
Alexander Tsankov <antsankov@gmail.com>
2014-11-15 08:31:19 -0700

    Refactored run

--- run.py
+++ run.py
@@ -6,10 +6,12 @@ import twilio.twiml
 from twilio.rest import TwilioRestClient 
 
 #our own classes 
-from persons import persons
+# from persons import persons
+# from bank import bank
+# from account import account
+# import transaction
 from bank import bank
-from account import account
-import transaction
+from parser import parser
 
 ACCOUNT_SID = "AC2a9e06ea97e4a1a785361f0e8064e870" 
 AUTH_TOKEN = "bc96d318cbda7ef495f1418baed042f4" 
@@ -27,71 +29,6 @@ def bank_init():
     # test_bank.addPerson('+17208378697', test_person)
     return test_bank
 
-def parser(origination_number,input,bank):
-    if(not(origination_number in bank.people.keys())):
-        test_person = persons(origination_number)
-        bank.add_person(origination_number)
-    else:
-        test_person = bank.get_person(origination_number)
-
-    #looks up the person from the bank based on their origination number
-    # user = bank.get_person(origination_number)
-    
-    #split the input 
-    mod_input = input.split()
-    verb = mod_input[0]
-    
-    #WITHDRAW
-    if (verb == "withdraw"):
-        accountID = mod_input[1]
-        amount = mod_input[2]
-
-        #check if the user has the account that they want to draw from 
-        if (accountID in test_person.waccounts ):
-            
-            #get the actual account from the bank 
-            account = bank.get_account(accountID)
-
-            if (hasFunds(amount, account)):
-                withdraw(account, test_person, amount)
-                return "SUCCESS " + input
-
-        else:
-            return "Invalid/Unknown account"
-
-
-    #DEPOSIT
-    if (verb == "deposit"):
-        accountID = mod_input[1]
-        amount = mod_input[2]
-
-        if (accountID in test_person.daccounts):
-            
-            account = bank.get_account(accountID)
-            deposit(account,test_person,amount)
-            return "SUCCESS " + input
-
-        else:
-            return "Invalid/Unknown account"
-    
-    #ADD       
-    if (verb == "add"):
-        phone_number = mod_input[1]
-        if (not(phone_number in bank.people.keys())):
-            bank.add_person(phone_number)
-            return ("Successfully added person with wallet of: " + str(bank.get_person(phone_number).address))
-        
-        else:
-            return "User has already been created"
-
-    #CREATE
-    if(verb == "create"):
-        phone_number = mod_input[1]
-        bank.add_account(phone_number)
-
-    else:
-        return "Unknown if withdrawl or deposit!"
-
 #this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
 def returner(bank,debug):
     messages = client.messages.list() 
@@ -101,23 +38,28 @@ def returner(bank,debug):
             return parser(m.from_,m.body,bank)
 
         if (m.direction == 'inbound' and debug == True):
-            #you need to use m.from_ NOT m.From, this causes 
+            #you need to use m.from_ NOT m.From, this causes keyword error  
             return m.body
  
-#this is the main route with the two possible verbs 
+#this is the main route with the two possible verbs, any methods after this runs automatically
 @app.route("/", methods=['GET', 'POST'])
+
+def tester():
+    #our m.from: +17208378697 m.body: what we want, bank
+    print(parser("+17208378697","add +17208378675",test_bank))
+    print(parser("+17208378697","add +17208378675",test_bank))
+
 
 #this is the responder function 
 def responder():
-    test_bank = bank_init()
     resp = twilio.twiml.Response()
     resp.message(returner(test_bank,False))
     return str(resp)
 
 
 @app.route("/debug", methods=['GET', 'POST'])
+
 def debugger():
-    test_bank = bank_init()
     resp = twilio.twiml.Response()
     resp.message(returner(test_bank,True))
     return str(resp)
@@ -125,6 +67,8 @@ def debugger():
 
 #this gets the server running.
 if __name__ == "__main__":
+    test_bank = bank_init()
+    tester()
     # print (parser("1111111","withdraw 555 48758475"))
     # print (parser("2222222","deposit 555 488923478923"))
     app.run(debug=True)
