b09f8678ce626b6ad55497e39dbda7bae72651d8
Alexander Tsankov <antsankov@gmail.com>
2014-11-15 11:01:48 -0700

    is it messages?

--- run.py
+++ run.py
@@ -25,100 +25,98 @@ def bank_init():
 
 
 def parser(origination_number,input,bank):
-    # if(not(origination_number in bank.people.keys())):
-    #     test_person = persons(origination_number)
-    #     bank.add_person(origination_number)
-    # else:
-    #     test_person = bank.get_person(origination_number)
+    if(not(origination_number in bank.people.keys())):
+        test_person = persons(origination_number)
+        bank.add_person(origination_number)
+    else:
+        test_person = bank.get_person(origination_number)
 
-    # #looks up the person from the bank based on their origination number
-    # # user = bank.get_person(origination_number)
+    #looks up the person from the bank based on their origination number
+    user = bank.get_person(origination_number)
     
-    # #split the input 
-    # mod_input = input.split()
-    # verb = mod_input[0]
-    # print("HELLO FUCK")
+    #split the input 
+    mod_input = input.split()
+    verb = mod_input[0]
+    print("HELLO FUCK")
     return "Fuck"
     
-#     #WITHDRAW
-#     if (verb == "withdraw"):
-#         accountID = mod_input[1]
-#         amount = mod_input[2]
+    #WITHDRAW
+    if (verb == "withdraw"):
+        accountID = mod_input[1]
+        amount = mod_input[2]
 
-#         #check if amount is valid
-#         amount = get_satoshis(amount)
-#         if(amount < 0):
-#             return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
+        #check if amount is valid
+        amount = get_satoshis(amount)
+        if(amount < 0):
+            return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
 
 
-#         #check if the user has the account that they want to draw from 
-#         if (accountID in test_person.waccounts ):
+        #check if the user has the account that they want to draw from 
+        if (accountID in test_person.waccounts ):
             
-#             #get the actual account from the bank 
-#             account = bank.get_account(accountID)
+            #get the actual account from the bank 
+            account = bank.get_account(accountID)
 
-#             if (hasFunds(amount, account)):
-#                 withdraw(account, test_person, amount)
-#                 return "SUCCESS " + input
+            if (hasFunds(amount, account)):
+                withdraw(account, test_person, amount)
+                return "SUCCESS " + input
 
-#         else:
-#             return "Invalid/Unknown account"
+        else:
+            return "Invalid/Unknown account"
 
 
-#     #DEPOSIT
-#     if (verb == "deposit"):
-#         accountID = mod_input[1]
-#         amount = mod_input[2]
+    #DEPOSIT
+    if (verb == "deposit"):
+        accountID = mod_input[1]
+        amount = mod_input[2]
 
-#         #check if amount is valid
-#         amount = get_satoshis(amount)
-#         if(amount < 0):
-#             return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
+        #check if amount is valid
+        amount = get_satoshis(amount)
+        if(amount < 0):
+            return "Invalid quantity specified: amount must be a valid currency valued at greater than 1 satoshi"
 
-#         if (accountID in test_person.daccounts):
+        if (accountID in test_person.daccounts):
             
-#             account = bank.get_account(accountID)
-#             deposit(account,test_person,amount)
-#             return "SUCCESS " + input
+            account = bank.get_account(accountID)
+            deposit(account,test_person,amount)
+            return "SUCCESS " + input
 
-#         else:
-#             return "Invalid/Unknown account"
+        else:
+            return "Invalid/Unknown account"
     
-#     #ADD       
-#     if (verb == "add"):
-#         phone_number = mod_input[1]
-#         if (not(phone_number in bank.people.keys())):
-#             bank.add_person(phone_number)
-#             return ("Successfully added person with wallet of: " + str(bank.get_person(phone_number).address))
+    #ADD       
+    if (verb == "add"):
+        phone_number = mod_input[1]
+        if (not(phone_number in bank.people.keys())):
+            bank.add_person(phone_number)
+            return ("Successfully added person with wallet of: " + str(bank.get_person(phone_number).address))
         
-#         else:
-#             return "User has already been created"
+        else:
+            return "User has already been created"
 
-#     #CREATE
-#     if(verb == "create"):
-#         phone_number = mod_input[1]
-#         id = bank.add_account(phone_number)
-#         return id 
-#     else:
-#         return "Unknown if withdrawl or deposit!"
-
-
+    #CREATE
+    if(verb == "create"):
+        phone_number = mod_input[1]
+        id = bank.add_account(phone_number)
+        return id 
+    else:
+        return "Unknown if withdrawl or deposit!"
 
 
 #this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
 def returner(bank,debug):
-    #messages = client.messages.list() 
-    # for m in messages:
-    #     if (m.direction == 'inbound' and debug == False):
-    #         #print("hello world returner")
-    #         #you need to use m.from_ NOT m.From, this causes 
-    #         #return parser(m.from_,m.body,bank)
-    #         return "shit"
-
-    #     if (m.direction == 'inbound' and debug == True):
-    #         #you need to use m.from_ NOT m.From, this causes keyword error  
-    #         return m.body
+    messages = client.messages.list() 
     return "FUCK"
+    for m in messages:
+        if (m.direction == 'inbound' and debug == False):
+            #print("hello world returner")
+            #you need to use m.from_ NOT m.From, this causes 
+            #return parser(m.from_,m.body,bank)
+            return "shit"
+
+        if (m.direction == 'inbound' and debug == True):
+            #you need to use m.from_ NOT m.From, this causes keyword error  
+            return m.body
  
  #this is the main route with the two possible verbs, any methods after this runs automatically
 @app.route("/", methods=['GET', 'POST'])
